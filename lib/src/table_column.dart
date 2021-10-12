1c605762c20e00b5a824e15263e32c30bd56617a
herbcookey <57701307+herbcookey@users.noreply.github.com>
2021-10-12 12:04:01 +0900

    Update table_column.dart

--- lib/src/table_column.dart
+++ lib/src/table_column.dart
@@ -65,7 +65,7 @@ class TableColumn extends StatelessWidget {
                             onRowTap(dataList!.indexOf(rowMap), rowMap);
                           },
                           child: Container(
-                            height:30,
+                            height: 25,
                             color: (allowRowHighlight &&
                                     highlightedRowIndex != null &&
                                     highlightedRowIndex ==
